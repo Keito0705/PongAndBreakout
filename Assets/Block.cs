ed5969f16e1c7bf7a4b6508ba3f682cdbf308bc5
Keito0705 <it232095@trident.ac.jp>
2025-04-16 19:35:19 +0900

    Pキーおしたらブロックの色情報とレイヤー情報が一つずつ右に行くようにした

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,48 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer; // SpriteRendererを保持するための変数
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       // SpriteRendererコンポーネントを取得
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-        // 初期状態でWhiteBlockレイヤーを設定（必要なら追加）
-        //gameObject.layer = LayerMask.NameToLayer("WhiteBlock");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int row;
+    public int col;
+    public BlockPlacer placer;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (placer == null) return;
+
         if (collision.gameObject.name == "WhiteBall")
         {
-            // 色を黒に変更
-            spriteRenderer.color = Color.black;
-
-            // レイヤーをBlackBlockに変更
-            gameObject.layer = LayerMask.NameToLayer("BlackBlock");
-
-            //Destroy(this.gameObject);
+            placer.SetBlockColorAndLayer(row, col, BlockPlacer.BlockColor.Black, "BlackBlock");
         }
-        if (collision.gameObject.name == "BlackBall")
+        else if (collision.gameObject.name == "BlackBall")
         {
-            // 色を黒に変更
-            spriteRenderer.color = Color.white;
-
-            // レイヤーをBlackBlockに変更
-            gameObject.layer = LayerMask.NameToLayer("WhiteBlock");
-
-            //Destroy(this.gameObject);
+            placer.SetBlockColorAndLayer(row, col, BlockPlacer.BlockColor.White, "WhiteBlock");
         }
     }
 }
--- Assets/BlockPlacer.cs
+++ Assets/BlockPlacer.cs
@@ -2,104 +2,121 @@ using UnityEngine;
 
 public class BlockPlacer : MonoBehaviour
 {
-    public GameObject blackBlockPrefab;
-    public GameObject whiteBlockPrefab;
+    public GameObject blockPrefab; // 白色のPrefabのみ
     public int rows = 5;
-    public int columns = 10;
+    public int columns = 10; // 片側の列数（全体はcolumns*2）
     public float spacing = 1.5f;
     public float initializePosx = 0.25f;
     public float initializePosy = 0.25f;
 
-    // どちらのPrefabかを記録する配列（true:黒, false:白）
-    private bool[,] isBlackBlock;
-    // 実際のブロックオブジェクト
     private GameObject[,] blocks;
+    private BlockColor[,] colorArray;
+    private string[,] layerArray;
+
+    public enum BlockColor { Black, White }
 
     void Start()
     {
-        isBlackBlock = new bool[rows, columns];
-        blocks = new GameObject[rows, columns];
+        int totalCols = columns * 2;
+        blocks = new GameObject[rows, totalCols];
+        colorArray = new BlockColor[rows, totalCols];
+        layerArray = new string[rows, totalCols];
 
         for (int i = 0; i < rows; i++)
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < totalCols; j++)
             {
                 Vector3 position = new Vector3(j * spacing + initializePosx, -i * spacing + initializePosy, 0);
-                GameObject prefab = (j < columns / 2) ? blackBlockPrefab : whiteBlockPrefab;
-                blocks[i, j] = Instantiate(prefab, position, Quaternion.identity, transform);
-                // 初期は左半分黒、右半分白
-                isBlackBlock[i, j] = (j < columns / 2);
+                blocks[i, j] = Instantiate(blockPrefab, position, Quaternion.identity);
+
+                Block blockScript = blocks[i, j].GetComponent<Block>();
+                if (blockScript != null)
+                {
+                    blockScript.row = i;
+                    blockScript.col = j;
+                    blockScript.placer = this;
+                }
+
+                if (j < columns)
+                {
+                    colorArray[i, j] = BlockColor.Black;
+                    layerArray[i, j] = "BlackBlock";
+                }
+                else
+                {
+                    colorArray[i, j] = BlockColor.White;
+                    layerArray[i, j] = "WhiteBlock";
+                }
             }
         }
-        UpdateBlockColors(); // 初期状態の色とレイヤーも反映
+        UpdateBlocks();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            ShiftColorsRight();
-            UpdateBlockColors();
+            ShiftRight();
+            UpdateBlocks();
         }
     }
 
-    // 色情報だけを右にシフト
-    void ShiftColorsRight()
+    public void SetBlockColorAndLayer(int row, int col, BlockColor color, string layerName)
     {
+        colorArray[row, col] = color;
+        layerArray[row, col] = layerName;
+        SetBlockColorAndLayer(blocks[row, col], color, layerName);
+    }
+
+    void ShiftRight()
+    {
+        int totalCols = columns * 2;
+        BlockColor[,] newColorArray = new BlockColor[rows, totalCols];
+        string[,] newLayerArray = new string[rows, totalCols];
+
         for (int i = 0; i < rows; i++)
         {
-            for (int j = columns - 1; j >= 0; j--)
+            for (int j = totalCols - 1; j >= 0; j--)
             {
                 if (j == 0)
                 {
-                    isBlackBlock[i, j] = true; // 一番左は黒
+                    newColorArray[i, j] = BlockColor.Black;
+                    newLayerArray[i, j] = "BlackBlock";
                 }
                 else
                 {
-                    isBlackBlock[i, j] = isBlackBlock[i, j - 1];
+                    newColorArray[i, j] = colorArray[i, j - 1];
+                    newLayerArray[i, j] = layerArray[i, j - 1];
                 }
             }
         }
+        colorArray = newColorArray;
+        layerArray = newLayerArray;
     }
 
-    // 配列の情報に基づいて色とレイヤーを更新
-    void UpdateBlockColors()
+    void UpdateBlocks()
     {
+        int totalCols = columns * 2;
         for (int i = 0; i < rows; i++)
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < totalCols; j++)
             {
-                var block = blocks[i, j];
-                if (block != null)
-                {
-                    if (isBlackBlock[i, j])
-                    {
-                        SetBlockColorAndLayer(block, Color.black, "BlackBlock");
-                    }
-                    else
-                    {
-                        SetBlockColorAndLayer(block, Color.white, "WhiteBlock");
-                    }
-                }
+                SetBlockColorAndLayer(blocks[i, j], colorArray[i, j], layerArray[i, j]);
             }
         }
     }
 
-    void SetBlockColorAndLayer(GameObject block, Color color, string layerName)
+    void SetBlockColorAndLayer(GameObject block, BlockColor color, string layerName)
     {
-        var renderer = block.GetComponent<Renderer>();
-        if (renderer != null)
+        var spriteRenderer = block.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
         {
-            renderer.material.color = color;
+            spriteRenderer.color = (color == BlockColor.Black) ? Color.black : Color.white;
         }
         int layer = LayerMask.NameToLayer(layerName);
         if (layer != -1)
         {
             block.layer = layer;
-        }
-        else
-        {
-            Debug.LogWarning($"Layer '{layerName}' が存在しません。");
         }
     }
 }
